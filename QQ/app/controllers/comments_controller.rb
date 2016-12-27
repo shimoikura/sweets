75dc783b72c3e993d3f9d0d26c6187dd57277f45
shimoikura <ikura554@gmail.com>
2016-12-27 03:17:11 +0000

    コメント機能実装

--- QQ/app/controllers/comments_controller.rb
+++ QQ/app/controllers/comments_controller.rb
@@ -25,16 +25,15 @@ class CommentsController < ApplicationController
   # POST /comments.json
   def create
     @comment = Comment.new(comment_params)
-redirect_to :back
-    # respond_to do |format|
-    #   # if @comment.save
-    #   #   format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-    #   #   format.json { render :show, status: :created, location: @comment }
-    #   # else
-    #   #   format.html { render :new }
-    #   #   format.json { render json: @comment.errors, status: :unprocessable_entity }
-    #   # end
-    # end
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.json { render :show, status: :created, location: @comment }
+      else
+        format.html { render :new }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   # PATCH/PUT /comments/1
