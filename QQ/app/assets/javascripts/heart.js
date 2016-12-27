2b17cf8d1f1555b24745a2592839b74addf6493b
shimoikura <ikura554@gmail.com>
2016-12-27 06:30:38 +0000

    お気に入り最後のやつだけ実装

--- QQ/app/assets/javascripts/heart.js
+++ QQ/app/assets/javascripts/heart.js
@@ -1,5 +1,6 @@
 $(function(){
   $('.heart2').hide();
+  $('.heart4').hide();
 $('.heart1').click(function(){
     $('.heart1').toggle();
     $('.heart2').toggle();
@@ -8,4 +9,11 @@ $('.heart2').click(function(){
     $('.heart1').toggle();
     $('.heart2').toggle();
 });
+
+$('.heart3').click(function(){
+  console.log("ssss");
+    $('.heart3').hide();
+    $('.heart4').show();
+});
+
 });
